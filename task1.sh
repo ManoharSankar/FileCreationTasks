fb05d182beaa178bdd5d7929a46a3ff43f8ae4e4
ManoharSankar <manoharsankar93@gmail.com>
2024-07-20 04:48:40 +0000

    Pushing first task file to the github server

---
+++ task1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch my_file.txt another_file.txt
+echo "Welcome to the world of ZenDevOps Training Program!" > my_file.txt
+echo "Topics covered under this taining Program are GIT,JENKINS,ANSIBLE,TERRAFORM and etc..!" > another_file.txt
+cat  another_file.txt >>  my_file.txt
+cat my_file.txt
+ls -l
